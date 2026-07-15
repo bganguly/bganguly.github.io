7451e6f6893b16399c3d018c70983b239f7e5409
bganguly <gangulybikramjit@hotmail.com>
2026-07-15 13:59:20 -0700

    deploy: update rag/lite live URL + flags

--- live-urls.js
+++ live-urls.js
@@ -5,7 +5,7 @@ window._liveUrls = {
   nextjsLiteFe: 'https://df9jh7fbcc9nk.cloudfront.net',
   nlsqlLiteFe: 'https://natural-language-to-llm-query-comparison.vercel.app/nl-to-sql/',
   ragLiteBe: 'http://localhost:8001/docs',
-  ragLiteFe: 'http://rag-lite-alb-592312982.us-east-1.elb.amazonaws.com',
+  ragLiteFe: 'https://d21n92v3nexm0p.cloudfront.net',
   serverlessBe: 'https://d281doisqbuiu2.cloudfront.net/api-explorer.html',
   serverlessFe: 'https://d281doisqbuiu2.cloudfront.net',
 };
